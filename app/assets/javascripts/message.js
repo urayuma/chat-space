151286469faba203b7591543c517018da86f152e
urayuma <orangesound@outlook.jp>
2019-11-02 20:05:16 +0900

    automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,22 +1,23 @@
-$(function(){
+$(document).ready(function(){
   function buildHTML(message){
-    var img = message.image ? message.image : "";
-    var html = `<div class="message">
-                  <div class="upper-message">
-                    <div class="upper-message__user-name">
-                      ${message.user_name}
-                    </div>
-                    <div class="upper-message__date">
-                      ${message.created_at}
-                    </div>
+
+    var img = message.image? `<img src="${message.image}">` : "";
+
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message"> 
+                    <div class="upper-message__user-name"> 
+                      ${message.user_name} 
+                    </div> 
+                    <div class="upper-message__date"> 
+                      ${message.created_at} 
+                    </div> 
+                  </div> 
+                  <div class="lower-message"> 
+                    ${message.content}
+                    ${img}
                   </div>
-                  <div class="lower-message">
-                   <p class="lower-message__content">
-                     ${message.content}
-                   </p>
-                  </div>
-                  <img class="lower-message__image" src=${img} >
-                </div>`
+                </div>`;
+
     return html;
   }
   $('.new_message').on('submit', function(e){
@@ -42,4 +43,36 @@ $(function(){
     });
     return false;
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("id")
+    var url = 'api/messages';
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: url,
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'GET',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 追加するHTMLの入れ物を作る
+      var insertHTML = "";
+          messages.forEach(function(message) {
+            insertHTML = buildHTML(message)
+          });
+          $(".messages").append(insertHTML);
+          $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+        })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if ($(".messages").length) {
+    autoReload = setInterval(reloadMessages, 5000);
+   } else {
+    clearInterval(autoReload);
+   }
 });
