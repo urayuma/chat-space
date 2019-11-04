f7723fd5afb72ef651d64461183b4bc886506ffc
urayuma <orangesound@outlook.jp>
2019-11-04 14:22:21 +0900

    line27_append_change_set

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, "git@github.com:urayuma/chat-space.git"
 # append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
